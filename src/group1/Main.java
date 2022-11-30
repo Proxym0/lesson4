68637ebd60e44c11a92d3fb014253866b9e83ebf
Proxym0 <vladi.shapovalov@gmail.com>
2022-11-30 22:03:36 +0300

    save2

--- src/group1/Main.java
+++ src/group1/Main.java
@@ -6,14 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-
         int[] nums = new int[]{1, 3, 4, 5, 63, 2, 45, 4};
-
         for (int i = 0; i < nums.length; i++) {
             if (a == nums[i]) {
                 System.out.println("число входит в массив под инжексом №"+i);
-
-
             }
         }
     }
