e7b4a75c807aaad44e8966757f6384f56e7a2a72
Proxym0 <vladi.shapovalov@gmail.com>
2022-12-03 22:04:09 +0300

    save9

--- src/group2/Class1.java
+++ src/group2/Class1.java
@@ -6,35 +6,35 @@ import java.util.Scanner;
 
 public class Class1 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int a=scanner.nextInt();
-        Random random=new Random();
-        int[] array=new int[a];
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        Random random = new Random();
+        int[] array = new int[a];
         int i;
         int evenNums = 0;
-        for(i=0;i<a;i++){
-            array[i]=random.nextInt(10);
-            if(array[i]%2 == 0){
-                evenNums++;
+        if (a < 5 || a > 10) {
+            System.out.println("array size is invalid");
+        } else {
+            System.out.println("");
+            for (i = 0; i < a; i++) {
+                array[i] = random.nextInt(10);
+                if (array[i] % 2 == 0) {
+                    evenNums++;
+                }
             }
-        }
-        System.out.println(Arrays.toString(array));
-        int [] array1 = new int[evenNums];
-        int index = 0;
-        for (i = 0; i < a; i++) {
-            if(array[i]%2 == 0){
-                array1[index] = array[i];
-                index++;
+            System.out.println(Arrays.toString(array));
+            int[] array1 = new int[evenNums];
+            int index = 0;
+            for (i = 0; i < a; i++) {
+                if (array[i] % 2 == 0) {
+                    array1[index] = array[i];
+                    index++;
+                }
             }
-        }
-        System.out.println(Arrays.toString(array1));
-        if(a<5||a>10) {
-                System.out.println("array size is invalid");
-            }else{
-                System.out.println("");
-            }
-
+            System.out.println(Arrays.toString(array1));
         }
     }
+}
+
 
 
