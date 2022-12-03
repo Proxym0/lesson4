9f2eeecf4e3330eeb3ce8486d9f9ea9a9e88a124
Proxym0 <vladi.shapovalov@gmail.com>
2022-12-03 23:02:13 +0300

    save13

---
+++ src/group2/Class4.java
@@ -0,0 +1,27 @@
+package group2;
+
+import java.util.Arrays;
+
+public class Class4 {
+    public static void main(String[] args) {
+        int [] array = {54, 21, 18, 4, 9};
+
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length-1; i++) {
+                if(array[i] > array[i+1]){
+                    isSorted = false;
+
+                    buf = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
+
+
