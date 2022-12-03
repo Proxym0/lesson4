bcec485b6de36374ce7463cf5f98418378102b06
Proxym0 <vladi.shapovalov@gmail.com>
2022-12-03 22:45:34 +0300

    save12

--- src/group2/Class3.java
+++ src/group2/Class3.java
@@ -1,4 +1,11 @@
 package group2;
 
+import java.util.Arrays;
+
 public class Class3 {
+    public static void main(String[] args) {
+        String[] words = {"Kaite","Vlad","Alex","Yuri"};
+        Arrays.sort(words);
+        System.out.println(Arrays.toString(words));
+    }
 }
