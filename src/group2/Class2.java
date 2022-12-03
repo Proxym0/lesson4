88334843722c13e6624f02c6799614276e47f378
Proxym0 <vladi.shapovalov@gmail.com>
2022-12-03 22:35:53 +0300

    save10

---
+++ src/group2/Class2.java
@@ -0,0 +1,25 @@
+package group2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Class2 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int[] array = new int[10];
+        int i;
+        int evenNums = 0;
+        for (i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10);
+        }
+        System.out.print(Arrays.toString(array));
+        System.out.println(" ");
+        for (i = 0; i < array.length; i++) {
+            if (i % 2 == 1) {
+                array[i] = 0;
+            }
+        }
+        System.out.print(Arrays.toString(array));
+    }
+}
